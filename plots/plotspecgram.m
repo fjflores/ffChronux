d529075af160aaf7db05cfc7d5d1e7f1616543a1
Francisco J. Flores <fjflores@mit.edu>
2025-02-20 23:15:42 -0500

    adds newer options to plto spectrograms.

--- plots/plotspecgram.m
+++ plots/plotspecgram.m
@@ -1,24 +1,28 @@
-function plot_matrix( X, t, f, plt, Xerr )
-% Function to plot a time-frequency matrix X. Time and frequency axes are in t and f.
-% If error bars are specified in Xerr,
-% it also plots them. Xerr contains upper and lower confidence intervals 
-% on X. 
-% 
-% Usage: plot_matrix(X,t,f,plt,Xerr)
-% 
+function varargout = plotspecgram( S, t, f, plt )
+% PLOTSPECGRAM spectrogram plot with options.
+%
+% Usage:
+% plotspecgram( S, t, f, plt, Serr )
+%
 % Inputs:
-% X: input vector as a function of time and frequency (t x f)
+% S: input vector as a function of time and frequency (t x f)
 % t: t axis grid for plot. Default [1:size(X,1)]
 % f: f axis grid for plot. Default. [1:size(X,2)]
-% plt: 'l' for log, 'n' for no log.
-% Xerr: lower and upper confidence intervals for X1: lower/upper x t x f.
+% plt: 'linear' for linear spectral power. "log" for logarithmic spectral 
+% power. "loglog" for logarithmic spectral power and logarithmic frequency.
+
+% Serr: lower and upper confidence intervals for X1: lower/upper x t x f.
+%
+
+
+
 
 if nargin < 1
     error( 'Need data' )
 
 end
 
-[ NT, NF ] = size( X );
+[ NT, NF ] = size( S );
 if nargin < 2
     t = 1 : NT;
 
@@ -30,52 +34,78 @@ if nargin < 3
 end
 
 if length( f ) ~= NF || length( t ) ~= NT
-    error('axes grid and data have incompatible lengths'); 
+    error('axes grid and data have incompatible lengths');
 
 end
 
 if nargin < 4 || isempty( plt )
-    plt = 'l';
+    plt = "logP";
 
 end
 
-if strcmp( plt, 'l' )
-    X = 10 * log10( X );
+hAx = axes;
+switch plt
+    case "linear"
+        imagesc( t, f, S' )
+        barLeg = "power (uV^2)";
+        axis xy
 
-    if nargin == 5
-        Xerr = 10 * log10( Xerr ); 
-    
-    end
+    case "log"
+        imagesc( t, f, 10 * log10( S' ) )
+        barLeg = "power (dB)";
+        axis xy
+
+    case "loglog"
+        varInfo = whos( 'S' );
+        byteSize = varInfo.bytes;
+
+        if byteSize < 10e6
+            t = t( 1 : 2 : end );
+            S = S( 1 : 2 : end, : );
+        
+        elseif byteSize >= 10e6 && byteSize < 100e6 % greater than 100 MB decimate by 2.
+            t = t( 1 : 6 : end );
+            S = S( 1 : 6 : end, : );
+
+        elseif byteSize >= 100e6 % greater than 100 MB decimate by 2.
+            t = t( 1 : 10 : end );
+            S = S( 1 : 10 : end, : );
+
+        end
+
+        pcolor( t, f, 10 * log10( S' ) )
+        shading flat
+        set( gca, "Yscale", "log" )
+        barLeg = "power (dB)";
 
 end
 
-if nargin < 5
-   imagesc( t, f, X' )
-   axis xy
-   colorbar
-   title( 'Spectrogram' )
+ffcbar( gcf, gca, barLeg )
 
-else
-   subplot( 311 )
-   imagesc( t, f, squeeze( Xerr( 1, :, : ) )' )
-   axis xy
-   colorbar
-   title( 'Lower confidence' )
-
-   subplot( 312 )
-   imagesc( t, f, X' )
-   title( 'X' )
-   axis xy
-   colorbar
-
-   subplot( 313 )
-   imagesc( t, f, squeeze( Xerr( 2, :, : ) )' )
-   axis xy
-   colorbar
-   title( 'Upper confidence' )
+if nargout > 0
+    varargout{ 1 } = hAx;
 
 end
+hold off
 
-xlabel( 't' )
-ylabel( 'f' )
+% title( 'Spectrogram' )
+
+% else
+%    subplot( 311 )
+%    imagesc( t, f, squeeze( Serr( 1, :, : ) )' )
+%    axis xy
+%    colorbar
+%    title( 'Lower confidence' )
+%
+%    subplot( 312 )
+%    imagesc( t, f, S' )
+%    title( 'X' )
+%    axis xy
+%    colorbar
+%
+%    subplot( 313 )
+%    imagesc( t, f, squeeze( Serr( 2, :, : ) )' )
+%    axis xy
+%    colorbar
+%    title( 'Upper confidence' )
 
